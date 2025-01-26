4bea4c01d22a6fadeaf8f804e0aef1c916470493
Stefano Stortini <stefano.stortini@limolane.com>
2025-01-26 23:14:35 +0100

    Linter auto tabbing and such

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,56 @@
-import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
-import { includeIgnoreFile } from '@eslint/compat';
 import svelte from 'eslint-plugin-svelte';
+import prettier from 'eslint-config-prettier';
+import { fixupPluginRules } from '@eslint/compat';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
+
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-	includeIgnoreFile(gitignorePath),
-	js.configs.recommended,
-	...svelte.configs['flat/recommended'],
-	prettier,
-	...svelte.configs['flat/prettier'],
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node
-			}
-		}
-	}
+  {
+    ignores: ['.svelte-kit', 'build', 'node_modules']
+  },
+  js.configs.recommended,
+  ...svelte.configs['flat/recommended'],
+  ...svelte.configs['flat/prettier'],
+  {
+    plugins: {
+      svelte: fixupPluginRules(svelte)
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 'latest'
+      }
+    }
+  },
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parser: svelte.parser
+    }
+  },
+  prettier,
+  {
+    rules: {
+      'svelte/no-at-html-tags': 'error',
+      'svelte/valid-compile': 'error',
+      'prettier/prettier': [
+        'error',
+        {
+          svelteStrictMode: true,
+          svelteBracketNewLine: false,
+          tabWidth: 2,
+          singleQuote: true,
+          trailingComma: 'es5'
+        }
+      ]
+    }
+  }
 ];
--- src/routes/Pagina2/+page.svelte
+++ src/routes/Pagina2/+page.svelte
@@ -1,125 +1,123 @@
 <script>
-    let accordionStates = {
-        tipologia_feedback: false,
-        personalizzazione_feedback: false,
-        notifiche: false,
-        personalizzazione_notifiche: false,
-        i_miei_feedback_notifiche: false
-    };
+	let accordionStates = {
+		tipologia_feedback: false,
+		personalizzazione_feedback: false,
+		notifiche: false,
+		personalizzazione_notifiche: false,
+		i_miei_feedback_notifiche: false,
+	};
 
-    let timeVisualization = 1;
+	let timeVisualization = 1;
 
-    function toggleAccordion(id) {
-        accordionStates[id] = !accordionStates[id];
-    }
+	function toggleAccordion(id) {
+		accordionStates[id] = !accordionStates[id];
+	}
 
-    function increaseTime() {
-        timeVisualization += 1;
-    }
+	function increaseTime() {
+		timeVisualization += 1;
+	}
 
-    function decreaseTime() {
-        if (timeVisualization > 0) {
-            timeVisualization -= 1;
-        }
-    }
+	function decreaseTime() {
+		if (timeVisualization > 0) {
+			timeVisualization -= 1;
+		}
+	}
 </script>
 
-
-
 <div class="bicappFilesManagement">
-    <header>
-      <div class="sideContainer">
-        <img src="img/logoenaming.svg" class="logo" />
-  
-        <div class="d-flex align-items-center">
-          <span class="fs_14 fw_400">Salvato!</span>
-          <img src="img/cloud.svg" class="ml-10" />
-        </div>
-  
-        <div class="d-flex align-items-center">
-          <img src="img/clock.svg" />
-          <img src="img/Vector.png" class="pointer ml-10" />
-        </div>
-  
-        <div class="d-flex align-items-center">
-          <img src="img/back_arrow.svg" />
-          <img src="img/forward_arrow.svg" />
-          <img src="img/Vector.png" class="pointer ml-10" />
-        </div>
-      </div>
-      
-      <div class="d-flex align-items-center pointer">
-        <span class="fs_20 fw_600">Nome progetto</span>
-        <img src="img/Vector.png" class="ml-10" />
-      </div>
-      
-      <div class="sideContainer">
-        <img src="img/profile_picture.svg" class="pointer" />
-        <img src="img/play.svg" class="pointer" />
-        <img src="img/download.svg" class="pointer" />
-  
-        <button class="btn btn-secondary btn-small">
-          <span>Esci</span>
-          <img src="img/Vector.png" class="pointer ml-10" />
-        </button>
-      </div>
-    </header>
-  
-    <div class="pageContainer">
-      <div class="sideBar">
-        <div class="tab top">
-          <img src="img/stile.svg" />
-          <span>Stile</span>
-        </div>
-        <div class="tab">
-          <img src="img/pagine.svg" />
-          <span>Pagine</span>
-        </div>
-        <div class="tab active">
-          <img src="img/componi.svg" />
-          <span>Componi</span>
-        </div>
-        <div class="tab">
-          <img src="img/caricamenti.svg" />
-          <span>Caricamenti</span>
-        </div>
-        <div class="tab">
-          <img src="img/libreria.svg" />
-          <span>Libreria</span>
-        </div>
-        <div class="tab">
-          <img src="img/impostazioni.svg" />
-          <span>Impostazioni</span>
-        </div>
-        <div class="tab"></div>
-      </div>
-  
-      <div class="content">
-        <div class="leftShoulder">
-          <div class="d-flex">
-            <h1>Feedback e Notifiche</h1>
-            <img src="img/info.svg" class="infoPoint" />
-          </div>
-  
-          <div class="sectionPage2">
-            <!-- Accordion Tipologia Feedback -->
-            <div class="accordion-item">
-              <div class="accordion-header" on:click={() => toggleAccordion('tipologia_feedback')}>
-                Tipologia di Feedback
-                <img src="img/freccia_basso.svg" class="accordion-arrow">
-              </div>
-              {#if accordionStates.tipologia_feedback}
-                <div id="tipologia_feedback" class="accordion_tipologia_feedback">
-                    <button>Visivo</button>
-                    <button>Testuale</button>
-                    <button>Sonoro</button>
-                    <button>Tattile</button>
-                </div>
-              {/if}
-            </div>
-  
-            <!-- Accordion Personalizzazione Feedback -->
-             <!--
+	<header>
+		<div class="sideContainer">
+			<img src="img/logoenaming.svg" class="logo" />
+
+			<div class="d-flex align-items-center">
+				<span class="fs_14 fw_400">Salvato!</span>
+				<img src="img/cloud.svg" class="ml-10" />
+			</div>
+
+			<div class="d-flex align-items-center">
+				<img src="img/clock.svg" />
+				<img src="img/Vector.png" class="pointer ml-10" />
+			</div>
+
+			<div class="d-flex align-items-center">
+				<img src="img/back_arrow.svg" />
+				<img src="img/forward_arrow.svg" />
+				<img src="img/Vector.png" class="pointer ml-10" />
+			</div>
+		</div>
+
+		<div class="d-flex align-items-center pointer">
+			<span class="fs_20 fw_600">Nome progetto</span>
+			<img src="img/Vector.png" class="ml-10" />
+		</div>
+
+		<div class="sideContainer">
+			<img src="img/profile_picture.svg" class="pointer" />
+			<img src="img/play.svg" class="pointer" />
+			<img src="img/download.svg" class="pointer" />
+
+			<button class="btn btn-secondary btn-small">
+				<span>Esci</span>
+				<img src="img/Vector.png" class="pointer ml-10" />
+			</button>
+		</div>
+	</header>
+
+	<div class="pageContainer">
+		<div class="sideBar">
+			<div class="tab top">
+				<img src="img/stile.svg" />
+				<span>Stile</span>
+			</div>
+			<div class="tab">
+				<img src="img/pagine.svg" />
+				<span>Pagine</span>
+			</div>
+			<div class="tab active">
+				<img src="img/componi.svg" />
+				<span>Componi</span>
+			</div>
+			<div class="tab">
+				<img src="img/caricamenti.svg" />
+				<span>Caricamenti</span>
+			</div>
+			<div class="tab">
+				<img src="img/libreria.svg" />
+				<span>Libreria</span>
+			</div>
+			<div class="tab">
+				<img src="img/impostazioni.svg" />
+				<span>Impostazioni</span>
+			</div>
+			<div class="tab"></div>
+		</div>
+
+		<div class="content">
+			<div class="leftShoulder">
+				<div class="d-flex">
+					<h1>Feedback e Notifiche</h1>
+					<img src="img/info.svg" class="infoPoint" />
+				</div>
+
+				<div class="sectionPage2">
+					<!-- Accordion Tipologia Feedback -->
+					<div class="accordion-item">
+						<div class="accordion-header" on:click={() => toggleAccordion('tipologia_feedback')}>
+							Tipologia di Feedback
+							<img src="img/freccia_basso.svg" class="accordion-arrow" />
+						</div>
+						{#if accordionStates.tipologia_feedback}
+							<div id="tipologia_feedback" class="accordion_tipologia_feedback">
+								<button>Visivo</button>
+								<button>Testuale</button>
+								<button>Sonoro</button>
+								<button>Tattile</button>
+							</div>
+						{/if}
+					</div>
+
+					<!-- Accordion Personalizzazione Feedback -->
+					<!--
             <div class="accordion-item">
               <div class="accordion-header" on:click={() => toggleAccordion('personalizzazione_feedback')}>
                 Personalizzazione del Feedback
@@ -200,76 +198,82 @@
             </div>
             {/if} 
             </div> -->
-  
-            <!-- Accordion Notifiche -->
-            <div class="accordion-item">
-              <div class="accordion-header" on:click={() => toggleAccordion('notifiche')}>
-                Notifiche
-                <img src="img/freccia_basso.svg" class="accordion-arrow">
-              </div>
-              {#if accordionStates.notifiche}
-              <div id="notifiche" class="accordion-content">
-                <p>Push</p>
-                <div class="sms">
-                  <div class="box-notification-sms">
-                    <div class="circle"></div>
-                    <div>
-                      <h4>Your message!</h4>
-                      <p class="paragraph-email">Lorem ipsum dolor sit amet..</p>
-                    </div>
-                    <div class="box-notification-sms-buttons">
-                      <button class="buttonStyle">Open</button>
-                      <button class="buttonStyle">Close</button>
-                    </div>
-                  </div>
-                </div>
-  
-                <p>Email</p>
-                <div class="email">
-                  <div class="box-notification-email">
-                    <div class="circle"></div>
-                    <div>
-                      <h4>Your message!</h4>
-                      <p class="paragraph-email">Lorem ipsum dolor sit amet..</p>
-                    </div>
-                  </div>
-                </div>
-  
-                <p>SMS</p>
-                <div class="sms">
-                  <div class="box-notification-sms">
-                    <div class="circle"></div>
-                    <div>
-                      <h4>Your message!</h4>
-                      <p class="paragraph-email">Lorem ipsum dolor sit amet..</p>
-                    </div>
-                  </div>
-                </div>
-  
-                <p>Vocali</p>
-                <div class="vocali">
-                  <div class="box-notification-vocali">
-                    <img src="img/Icon_vocale.svg" alt="Icon_vocale">
-                    <p>Notifica vocale personalizzata</p>
-                  </div>
-                  <div class="volume-control-container">
-                    <div class="volume-control">
-                      <img src="img/volume_low.svg" alt="volume-low" class="volume-icon">
-                      <input type="range" min="0" max="100" value="50" class="slider" id="volume-slider">
-                      <img src="img/volume_high.svg" alt="volume-high" class="volume-icon">
-                    </div>
-                    <button class="sound-btn">
-                      Personalizza suono
-                      <img src="img/iconpers_sound.svg" alt="iconpers_sound"> 
-                    </button>
-                  </div>
-                </div>
-              </div>
-              {/if}
-            </div>
-  
-            <!-- Accordion Personalizzazione Notifiche -->
-            <!-- <div class="accordion-item">
+
+					<!-- Accordion Notifiche -->
+					<div class="accordion-item">
+						<div class="accordion-header" on:click={() => toggleAccordion('notifiche')}>
+							Notifiche
+							<img src="img/freccia_basso.svg" class="accordion-arrow" />
+						</div>
+						{#if accordionStates.notifiche}
+							<div id="notifiche" class="accordion-content">
+								<p>Push</p>
+								<div class="sms">
+									<div class="box-notification-sms">
+										<div class="circle"></div>
+										<div>
+											<h4>Your message!</h4>
+											<p class="paragraph-email">Lorem ipsum dolor sit amet..</p>
+										</div>
+										<div class="box-notification-sms-buttons">
+											<button class="buttonStyle">Open</button>
+											<button class="buttonStyle">Close</button>
+										</div>
+									</div>
+								</div>
+
+								<p>Email</p>
+								<div class="email">
+									<div class="box-notification-email">
+										<div class="circle"></div>
+										<div>
+											<h4>Your message!</h4>
+											<p class="paragraph-email">Lorem ipsum dolor sit amet..</p>
+										</div>
+									</div>
+								</div>
+
+								<p>SMS</p>
+								<div class="sms">
+									<div class="box-notification-sms">
+										<div class="circle"></div>
+										<div>
+											<h4>Your message!</h4>
+											<p class="paragraph-email">Lorem ipsum dolor sit amet..</p>
+										</div>
+									</div>
+								</div>
+
+								<p>Vocali</p>
+								<div class="vocali">
+									<div class="box-notification-vocali">
+										<img src="img/Icon_vocale.svg" alt="Icon_vocale" />
+										<p>Notifica vocale personalizzata</p>
+									</div>
+									<div class="volume-control-container">
+										<div class="volume-control">
+											<img src="img/volume_low.svg" alt="volume-low" class="volume-icon" />
+											<input
+												type="range"
+												min="0"
+												max="100"
+												value="50"
+												class="slider"
+												id="volume-slider" />
+											<img src="img/volume_high.svg" alt="volume-high" class="volume-icon" />
+										</div>
+										<button class="sound-btn">
+											Personalizza suono
+											<img src="img/iconpers_sound.svg" alt="iconpers_sound" />
+										</button>
+									</div>
+								</div>
+							</div>
+						{/if}
+					</div>
+
+					<!-- Accordion Personalizzazione Notifiche -->
+					<!-- <div class="accordion-item">
               <div class="accordion-header" on:click={() => toggleAccordion('personalizzazione_notifiche')}>
                 Personalizzazione delle Notifiche
                 <img src="img/freccia_basso.svg" class="accordion-arrow">
@@ -381,83 +385,83 @@
               </div>
               { /if}
             </div> -->
-  
-            <!-- Accordion Feedback e Notifiche -->
-            <div class="accordion-item">
-                <div class="accordion-header" on:click={() => toggleAccordion('i_miei_feedback_notifiche')}>
-                    I miei feedback e notifiche
-                    <img src="img/freccia_basso.svg" class="accordion-arrow">
-                </div>
-                {#if accordionStates.i_miei_feedback_notifiche}
-                    <div id="i_miei_feedback_notifiche" class="accordion-content">
-                        <p class="feedback-text" style="margin-bottom: 20px;">Feedback</p>
-                        <div class="myfeedback">
-                            <div class="box-myfeedback">
-                                <img src="img/Myfeedback.svg" alt="myfeedback">
-                            </div>
-                        </div>
 
-                        <p class="notifiche-text" style="margin-top:70px;">Notifiche</p>
-                        <div class="mynotifiche">
-                            <div class="box-mynotifiche">
-                                <img src="img/icona_notifica.svg" alt="notifiche">
-                                <img src="img/text_notifica.svg" alt="text-notifiche">
-                            </div>
-                        </div>
+					<!-- Accordion Feedback e Notifiche -->
+					<div class="accordion-item">
+						<div
+							class="accordion-header"
+							on:click={() => toggleAccordion('i_miei_feedback_notifiche')}>
+							I miei feedback e notifiche
+							<img src="img/freccia_basso.svg" class="accordion-arrow" />
+						</div>
+						{#if accordionStates.i_miei_feedback_notifiche}
+							<div id="i_miei_feedback_notifiche" class="accordion-content">
+								<p class="feedback-text" style="margin-bottom: 20px;">Feedback</p>
+								<div class="myfeedback">
+									<div class="box-myfeedback">
+										<img src="img/Myfeedback.svg" alt="myfeedback" />
+									</div>
+								</div>
 
-                    </div>
+								<p class="notifiche-text" style="margin-top:70px;">Notifiche</p>
+								<div class="mynotifiche">
+									<div class="box-mynotifiche">
+										<img src="img/icona_notifica.svg" alt="notifiche" />
+										<img src="img/text_notifica.svg" alt="text-notifiche" />
+									</div>
+								</div>
+							</div>
+						{/if}
+					</div>
 
-                { /if}
-            </div>
-  
-            <!-- <button class="button_feedback">Salva modifiche</button> -->
-          </div>
-        </div>
-      </div>
-  
-      <div class="centeredContent content">
-        <div class="mobilePreview"></div>
-        <div class="pageNumberContainer">
-          <span class="fw_400">1/5</span>
-          <img src="img/Vector.png" class="ml-10" />
-        </div>
-        <div class="footer">
-          <div class="d-flex align-items-center">
-            <div class="zoom">
-              <input id="zoom_input" type="range" min="0" max="100" step="1" />
-            </div>
-            <img src="img/griglia.svg" class="mx-55 pointer" />
-            <img src="img/help.svg" class="pointer" />
-          </div>
-        </div>
-      </div>
-    </div>
+					<!-- <button class="button_feedback">Salva modifiche</button> -->
+				</div>
+			</div>
+		</div>
+
+		<div class="centeredContent content">
+			<div class="mobilePreview"></div>
+			<div class="pageNumberContainer">
+				<span class="fw_400">1/5</span>
+				<img src="img/Vector.png" class="ml-10" />
+			</div>
+			<div class="footer">
+				<div class="d-flex align-items-center">
+					<div class="zoom">
+						<input id="zoom_input" type="range" min="0" max="100" step="1" />
+					</div>
+					<img src="img/griglia.svg" class="mx-55 pointer" />
+					<img src="img/help.svg" class="pointer" />
+				</div>
+				<div></div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <style scoped>
+	.sectionPage2 {
+		overflow: auto;
+		height: calc(100vh - 200px);
+	}
 
-.sectionPage2{
-    overflow: auto;
-    height: calc(100vh - 200px);
-}
+	.accordion_tipologia_feedback {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 
-.accordion_tipologia_feedback{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
+	.accordion_tipologia_feedback button {
+		padding: 10px;
+		margin: 10px;
+		width: 75%;
+	}
 
-.accordion_tipologia_feedback button{
-    padding: 10px;
-    margin: 10px;
-    width: 75%;
-}
-
-.box-notification-sms-buttons{
- display: flex;
- flex-direction: column;
- gap: 8px;
- margin-left: auto;
- margin-right: 8px;
-}
+	.box-notification-sms-buttons {
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+		margin-left: auto;
+		margin-right: 8px;
+	}
 </style>
